88bda060207386f721c021209cb354252ebad4c6
krsns <98344484+krsns@users.noreply.github.com>
2025-12-03 10:39:44 +0700

    Create logic.py

---
+++ src/logic.py
@@ -0,0 +1,6 @@
+def generate_response(msg: str):
+    # Logic agen kamu bisa kamu kembangkan
+    if "halo" in msg.lower():
+        return "Halo juga! Ada yang bisa saya bantu?"
+
+    return f"Kamu mengirim: {msg}"
