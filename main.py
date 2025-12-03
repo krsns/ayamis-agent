e8d73921e6c4442d40ddcca207d3a311756936d5
krsns <98344484+krsns@users.noreply.github.com>
2025-12-03 10:45:31 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"status": "Agent Ayamis is running!"}
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
